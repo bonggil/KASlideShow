04478a06ce7286e5db9aaea442868d668dc1fde5
ed <ed@dunamu.com>
2019-09-27 11:10:23 +0900

    Fix Build warning 'Block implicitly retains 'self'; explicitly mention 'self' to indicate this is intended behavior'

--- KASlideShow/KASlideShow.m
+++ KASlideShow/KASlideShow.m
@@ -249,7 +249,7 @@ typedef NS_ENUM(NSInteger, KASlideShowSlideMode) {
         if([delegate respondsToSelector:@selector(kaSlideShowDidShowNext:)]){
             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, transitionDuration * NSEC_PER_SEC);
             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                [delegate kaSlideShowDidShowNext:self];
+                [self->delegate kaSlideShowDidShowNext:self];
             });
         }
     }
@@ -298,7 +298,7 @@ typedef NS_ENUM(NSInteger, KASlideShowSlideMode) {
         if([delegate respondsToSelector:@selector(kaSlideShowDidShowPrevious:)]){
             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, transitionDuration * NSEC_PER_SEC);
             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                [delegate kaSlideShowDidShowPrevious:self];
+                [self->delegate kaSlideShowDidShowPrevious:self];
             });
         }
     }
@@ -311,22 +311,22 @@ typedef NS_ENUM(NSInteger, KASlideShowSlideMode) {
     
     [UIView animateWithDuration:transitionDuration
                      animations:^{
-                         _topImageView.alpha = 0;
+                         self->_topImageView.alpha = 0;
                      }
                      completion:^(BOOL finished){
                          
-                         _topImageView.image = _bottomImageView.image;
-                         _topImageView.alpha = 1;
+                         self->_topImageView.image = _bottomImageView.image;
+                         self->_topImageView.alpha = 1;
                          
-                         _isAnimating = NO;
+                         self->_isAnimating = NO;
                          
-                         if ([delegate respondsToSelector:@selector(slideShow:completeImageView:)]) {
-                             [delegate slideShow:self completeImageView:_topImageView];
+                         if ([self->delegate respondsToSelector:@selector(slideShow:completeImageView:)]) {
+                             [self->delegate slideShow:self completeImageView:_topImageView];
                          }
                          
-                         if(! _doStop){
+                         if(! self->_doStop){
                              [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(next) object:nil];
-                             [self performSelector:@selector(next) withObject:nil afterDelay:delay];
+                             [self performSelector:@selector(next) withObject:nil afterDelay:self->delay];
                          }
                      }];
 }
@@ -347,37 +347,37 @@ typedef NS_ENUM(NSInteger, KASlideShowSlideMode) {
                               animations:^{
                                   [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.6 animations:^{
                                       if(mode == KASlideShowSlideModeBackward){
-                                          _topImageView.transform = CGAffineTransformMakeTranslation( _topImageView.frame.size.width * 0.9, 0);
-                                          _bottomImageView.transform = CGAffineTransformMakeTranslation(- _bottomImageView.frame.size.width * 0.1, 0);
+                                          self->_topImageView.transform = CGAffineTransformMakeTranslation( self->_topImageView.frame.size.width * 0.9, 0);
+                                          self->_bottomImageView.transform = CGAffineTransformMakeTranslation(- self->_bottomImageView.frame.size.width * 0.1, 0);
                                       }else if(mode == KASlideShowSlideModeForward){
-                                          _topImageView.transform = CGAffineTransformMakeTranslation(- _topImageView.frame.size.width * 0.9, 0);
-                                          _bottomImageView.transform = CGAffineTransformMakeTranslation(_bottomImageView.frame.size.width * 0.1, 0);
+                                          self->_topImageView.transform = CGAffineTransformMakeTranslation(- self->_topImageView.frame.size.width * 0.9, 0);
+                                          self->_bottomImageView.transform = CGAffineTransformMakeTranslation(self->_bottomImageView.frame.size.width * 0.1, 0);
                                       }
                                   }];
                                   
                                   [UIView addKeyframeWithRelativeStartTime:0.6 relativeDuration:0.4 animations:^{
                                       if(mode == KASlideShowSlideModeBackward){
-                                          _topImageView.transform = CGAffineTransformMakeTranslation( _topImageView.frame.size.width, 0);
-                                          _bottomImageView.transform = CGAffineTransformMakeTranslation(0, 0);
+                                          self->_topImageView.transform = CGAffineTransformMakeTranslation( self->_topImageView.frame.size.width, 0);
+                                          self->_bottomImageView.transform = CGAffineTransformMakeTranslation(0, 0);
                                       }else if(mode == KASlideShowSlideModeForward){
-                                          _topImageView.transform = CGAffineTransformMakeTranslation(- _topImageView.frame.size.width, 0);
-                                          _bottomImageView.transform = CGAffineTransformMakeTranslation(0, 0);
+                                          self->_topImageView.transform = CGAffineTransformMakeTranslation(- self->_topImageView.frame.size.width, 0);
+                                          self->_bottomImageView.transform = CGAffineTransformMakeTranslation(0, 0);
                                       }
                                   }];
 
                               } completion:^(BOOL finished) {
-                                  _topImageView.image = _bottomImageView.image;
-                                  _topImageView.transform = CGAffineTransformMakeTranslation(0, 0);
+                                  self->_topImageView.image = self->_bottomImageView.image;
+                                  self->_topImageView.transform = CGAffineTransformMakeTranslation(0, 0);
                                   
-                                  _isAnimating = NO;
+                                  self->_isAnimating = NO;
                                   
-                                  if ([delegate respondsToSelector:@selector(slideShow:completeImageView:)]) {
-                                      [delegate slideShow:self completeImageView:_topImageView];
+                                  if ([self->delegate respondsToSelector:@selector(slideShow:completeImageView:)]) {
+                                      [self->delegate slideShow:self completeImageView:self->_topImageView];
                                   }
                                   
-                                  if(! _doStop){
+                                  if(! self->_doStop){
                                       [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(next) object:nil];
-                                      [self performSelector:@selector(next) withObject:nil afterDelay:delay];
+                                      [self performSelector:@selector(next) withObject:nil afterDelay:self->delay];
                                   }
                               }];
 }
